e729f2e3b2345507a6653809b97eb42519f0fb96
Codejijo <jijojohnlikeu@gmail.com>
2026-02-14 01:55:50 +0530

    feat : Added Image viewer window

--- src/windows/index.js
+++ src/windows/index.js
@@ -3,5 +3,6 @@ import Safari from "./Safari";
 import Resume from "./Resume";
 import Finder from "./Finder";
 import Text from "./Text";
+import Photo from "./Photo";
 
-export { Terminal, Safari, Resume, Finder, Text };
+export { Terminal, Safari, Resume, Finder, Text, Photo };
